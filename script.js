e850e2699eaad05e2ca67edf4da1886016f0fe3f
Allison Loan Rodrigues Ribeiro <allisonloanrodrigues@gmail.com>
2026-09-20 01:09:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const cursorGlow = document.querySelector('.cursor-glow');
+
+let mouseX = -100;
+let mouseY = -100;
+let glowX = -100;
+let glowY = -100;
+
+document.addEventListener('mousemove', (event) => {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+});
+
+function animateCursor() {
+  glowX += (mouseX - glowX) * 0.16;
+  glowY += (mouseY - glowY) * 0.16;
+  cursorGlow.style.transform = `translate3d(${glowX}px, ${glowY}px, 0)`;
+  requestAnimationFrame(animateCursor);
+}
+
+animateCursor();
+
+document.querySelectorAll('a, .button, .project').forEach((element) => {
+  element.addEventListener('mouseenter', () => cursorGlow.classList.add('is-hovering'));
+  element.addEventListener('mouseleave', () => cursorGlow.classList.remove('is-hovering'));
+});
